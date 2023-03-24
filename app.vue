ecda753c02807412f37580f1da743d49b168225c
Omar Siam <simar@gmx.net>
2023-03-24 20:13:18 +0100

    Add code from vue-winbox playground example
    Needs some stylings on top/app level
    Uses less.js for css, needs configuration for less.js

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
+<script>
+  // import min from 'winbox/src/img/min.svg'
+  // import max from 'winbox/src/img/max.svg'
+  // import close from 'winbox/src/img/close.svg'
+  // import full from 'winbox/src/img/full.svg'
+  // import restore from 'winbox/src/img/full.svg'
+  // import exit from 'winbox/src/img/exit.svg'
+</script>
+
 <template>
   <div>
     <NuxtPage />
   </div>
 </template>
+
+<style lang="less">
+@import 'winbox/src/css/winbox.less';
+@import 'winbox/src/css/themes/modern.less';
+// See https://github.com/wobsoriano/vue-winbox/blob/master/playground/src/App.vue
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  height: 100vh;
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica, Arial, "Open Sans", OpenSans, Roboto, Segoe UI, sans-serif;
+  background: linear-gradient(135deg, #0d1117, #131820);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,89 @@
-<script lang="ts" setup></script>
+<!-- See https://github.com/wobsoriano/vue-winbox/blob/master/playground/src/App.vue -->
+<script setup lang="ts">
+
+const options = {
+  title: 'Count: 0',
+  class: 'modern',
+  x: 'center',
+  y: 'center',
+  width: '50%',
+  height: '50%',
+}
+const winboxRef = ref()
+const isOpen = ref(true)
+
+const setTitle = (count: number) => {
+  winboxRef.value?.winbox?.setTitle(`Count: ${count}`)
+}
+
+// TODO: Check winbox status before resizing
+// const handleResize = () => {
+//   winboxRef.value?.winbox?.resize("50%", "50%").move("center", "center")
+// }
+
+// onMounted(() => {
+//   window.addEventListener('resize', handleResize)
+// })
+
+// onUnmounted(() => {
+//   window.removeEventListener('resize', handleResize)
+// })
+
+const initialize = () => {
+  winboxRef.value?.initialize()
+}
+
+const openUrl = async () => {
+  const randomId = Math.floor(Math.random() * 20) + 1
+  const createWinBox = await useWinBox()
+  createWinBox({
+    title: `Fox #${randomId}`,
+    url: `https://randomfox.ca/images/${randomId}.jpg`,
+    class: 'modern',
+  })
+}
+</script>
 
 <template>
-  <div>
-    Page: foo
+  <div>  
+    <VicavWinBox
+    ref="winboxRef"
+    :options="options"
+    @focus="isOpen = true"
+    @close="isOpen = false"
+  >
+    <Counter @update:count="setTitle" />
+  </VicavWinBox>
+  <div class="container">
+    <div v-show="!isOpen" class="button" @click="initialize">
+      Open Vue component
+    </div>
+    <div class="button" style="margin-top: 10px;" @click="openUrl">
+      Open Random URL
+    </div>
+  </div>
   </div>
 </template>
 
-<style scoped></style>
+<!-- See ../app.vue for global styles -->
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.button {
+  padding: 5px 10px;
+  margin-bottom: 5px;
+  display: inline-block;
+  background-color: #ca00b4;
+  color: #fff;
+  width: auto;
+  border-radius: 10px;
+  padding: 15px 25px;
+  cursor: pointer;
+}
+</style>
