65acb5c692505687b48cf27a9ade5a84dd8b273b
Omar Siam <simar@gmx.net>
2023-03-25 21:01:28 +0100

    Add a component that loads an image from a remote source
    This is a much simplified version of what happens if
    an image URL is passed to WinBox (iframe).
    Loading is not as smooth if the image is not in the cache.
    For the demo it is probably good enough

---
+++ components/RemoteImage.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { stringLiteral } from '@babel/types';
+
+const props = defineProps({
+  src: {
+    type: String,
+    required: true
+  },
+  alt: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div data-type="RemoteImage">
+    <img :src="src" :alt="alt"/>
+  </div>
+</template>
+
+<style scoped></style>
